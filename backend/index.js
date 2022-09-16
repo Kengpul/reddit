000a33502e705b62f2c36c58f9f2a2859e067ad5
kengpul <paulpulgares24@gmail.com>
2022-09-16 17:48:25 +0800

    setup mongo atlas

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const dbUrl = process.env.MONGO_URI || 'mongodb://localhost:27017/reddit';
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
 
 app.get('/', (req, res) => {
-    res.send('Welcome')
+    res.json({ 'name': 'paul' });
 })
 
 app.listen(3000, () => {
-    console.log('listening of Port 3000');
+    console.log('Listening of Port 3000');
 })
